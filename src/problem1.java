1b812bf14d309bd67a4adc17c53238122bf2b014
JamRamPage <48534848+JamRamPage@users.noreply.github.com>
2020-12-01 22:48:07 +0000

    Problem 1 - 10 minutes ish?

---
+++ src/problem1.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class problem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> numbers = parseFile("problem1.text");
+		System.out.println();
+		boolean done = false;
+		for(int a = 0; a < numbers.size() ; a++) {
+			int nA = numbers.get(a);
+			for(int b = 0; b < numbers.size() ; b++) {
+				int nB = numbers.get(b);
+				if (nA + nB == 2020) {
+					System.out.println(nA);
+					System.out.println(nB);
+					System.out.println(nA * nB);
+					done = !done;
+					break;
+				}
+			}
+			if(done) {
+				break;
+			}
+		}
+ 	}
+	
+	public static List<Integer> parseFile(String file) throws IllegalArgumentException {
+		// ArrayList holds the data in the file:
+		List<Integer> numbers = new ArrayList<Integer>();
+		BufferedReader buffer = null;
+		try {
+			FileReader in = new FileReader(file);
+			buffer = new BufferedReader(in);
+			String nextLine = buffer.readLine();
+			while(nextLine != null) {
+				nextLine = buffer.readLine();
+				if(nextLine != null) {
+					numbers.add(Integer.parseInt(nextLine));
+				}
+			}
+			// Handles errors when reading the file:
+		} catch (FileNotFoundException e) {
+			System.out.println("Cannot find the file");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Cannot read the file");
+			e.printStackTrace();
+		} finally {
+			if (buffer != null) {
+				try {
+					buffer.close();
+				} catch (IOException e) {
+					System.out.println("Could not close the file");
+					e.printStackTrace();
+				}
+			}
+		}
+		return numbers;
+	}
+	
+}
