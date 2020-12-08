771104a50d871b8481b945e78d9f16eaf669cd36
James G Page <James.Page@ibm.com>
2020-12-08 18:28:53 +0000

    Day 8 Done :)

--- src/problem16.java
+++ src/problem16.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class problem16 {
 
+	
 	public static void main(String[] args) {
 		List<String> instructions = parseFile("Day8.text");
 		boolean[] visitedInstructions = new boolean[instructions.size()];
@@ -22,16 +23,52 @@ public class problem16 {
 				accumulator = acc(accumulator, operand);
 				programCounter++;
 			}else if (opcode.equals("jmp")) {
+				//Try swapping with a nop and tracing out
+				int x = traceProgram(instructions, accumulator, programCounter, visitedInstructions, "nop", operand);
+				if (x != 0) {
+					// We terminated!
+					System.out.println(x);
+					break;
+				}
+				programCounter = jmp(programCounter, operand);
+			}else if (opcode.equals("nop")) {
+				//Try swapping with a jmp and tracing out
+				int x = traceProgram(instructions, accumulator, programCounter, visitedInstructions, "jmp", operand);
+				if (x != 0) {
+					// We terminated!
+					System.out.println(x);
+					break;
+				}
+				nop();
+				programCounter++;
+			}
+		}
+	}
+
+	public static int traceProgram(List<String> instructions, int accumulator, int programCounter, boolean[] visitedInstructions, String opcode, String operand) {
+		visitedInstructions[programCounter] = false;
+		while (programCounter < instructions.size() && !visitedInstructions[programCounter]) {
+			visitedInstructions[programCounter] = true;
+			// Execute the appropriate instruction.
+			if (opcode.equals("acc")) {
+				accumulator = acc(accumulator, operand);
+				programCounter++;
+			}else if (opcode.equals("jmp")) {
 				programCounter = jmp(programCounter, operand);
 			}else if (opcode.equals("nop")) {
 				nop();
 				programCounter++;
 			}
-			if (visitedInstructions[programCounter]) {
-				System.out.println();
+			// get the opcode and operand of the current instruction based on the PC
+			if (programCounter < instructions.size()) {
+				opcode = getOpcode(instructions.get(programCounter));
+				operand = getOperand(instructions.get(programCounter));
 			}
 		}
-		System.out.println(accumulator);
+		if (programCounter >= instructions.size()) {
+			return accumulator;
+		}
+		return 0;		
 	}
 
 	public static String getOpcode(String instruction) {
